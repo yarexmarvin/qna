aafc9558a9d7b8cd4d4f6dbceb7fa3bf157b6355
Yarex Marvin <yarexmarvin@yahoo.com>
2022-12-05 15:04:21 +0400

    fixed migrations

--- db/migrate/20221203125450_add_user_to_question.rb
+++ db/migrate/20221203125450_add_user_to_question.rb
@@ -1,5 +1,5 @@
 class AddUserToQuestion < ActiveRecord::Migration[7.0]
   def change
-    add_reference :questions, :user, null: false, foreign_key: true
+    add_reference :questions, :user, index: true, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_125511) do
     t.bigint "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_125511) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
